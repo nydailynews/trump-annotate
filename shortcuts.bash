e9dab3f4f6e7377fd51c8b240d3a0d77be849902
Kelli Parker <iamkelliparker@gmail.com>
2018-01-16 13:12:06 -0500

    setting up new project

---
+++ shortcuts.bash
@@ -0,0 +1,3 @@
+alias deployqa='cd ~/work/annotate/www; scp -r . qa:/apps/project/trump-victory-speech/'
+
+alias deployprod='cd ~/work/annotate/www; scp -r . prod:/apps/project/trump-victory-speech/'
